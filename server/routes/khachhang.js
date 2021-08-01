005643e466184b83ce2c0cf5a754fa61549243f2
minhtien <username@gmail.com>
2021-08-01 13:52:46 +0700

    fix error deploy

--- server/routes/khachhang.js
+++ server/routes/khachhang.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const controllerKhachHang = require("../controllers/khachHang");
+const controllerKhachHang = require("../controllers/khachhang");
 const router = express.Router();
 
 router.route("/").get(controllerKhachHang.getAllKhachHang);
